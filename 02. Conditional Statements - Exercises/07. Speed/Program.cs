5890c8449f32a0f1fd419de602f709daaddcc370
imp111 <alexsandartenev@gmail.com>
2024-09-21 22:43:15 +0300

    7 and 8 added

--- 02. Conditional Statements - Exercises/07. Speed/Program.cs
+++ 02. Conditional Statements - Exercises/07. Speed/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int speed = int.Parse(Console.ReadLine());
+string output = string.Empty;
+
+if (speed <= 10) output = "slow";
+else if (speed > 10 && speed <= 60) output = "average";
+else if (speed > 60 && speed <= 120) output = "fast";
+else if (speed > 120 && speed <= 160) output = "super-fast";
+else if (speed > 160) output = "turbo-fast";
+else output = "invalid speed";
+
+Console.WriteLine(output);
---
+++ 02. Conditional Statements - Exercises/08. Alarm after 15 minutes/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Hour: ");
+int hours = int.Parse(Console.ReadLine());
+
+while (hours < 0 || hours > 23)
+{ 
+    Console.Write("Invalid hour, try again!\nHour: ");
+    hours = int.Parse((Console.ReadLine()));
+}
+
+Console.Write("Minutes: ");
+int minutes = int.Parse(Console.ReadLine());
+
+while (minutes < 0 || minutes > 59)
+{
+    Console.WriteLine("Invalid minutes, try again!\nMinutes: ");
+    minutes = int.Parse((Console.ReadLine()));
+}
+
+minutes += 15;
+
+if (minutes > 59 && hours == 23)
+{
+    hours = 0;
+    minutes -= 60;
+}
+else if (minutes > 59)
+{
+    hours += 1;
+    minutes -= 60;
+}
+
+if (minutes >= 0 && minutes < 10)
+{
+    Console.WriteLine($"After 15 minutes added: {hours}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"After 15 minutes added: {hours}:{minutes}");
+}
