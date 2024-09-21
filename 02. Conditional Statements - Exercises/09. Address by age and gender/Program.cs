7cc9df0310ba6d674de79afca8f959ad60e630b6
imp111 <alexsandartenev@gmail.com>
2024-09-21 22:50:51 +0300

    09 added and done

---
+++ 02. Conditional Statements - Exercises/09. Address by age and gender/Program.cs
@@ -0,0 +1,28 @@
+﻿int age = int.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine());
+string output = string.Empty;
+
+if (gender == 'f')
+{
+    if (age <= 16)
+    {
+        output = "Miss";
+    }
+    else
+    {
+        output = "Ms.";
+    }
+}
+else if (gender == 'f')
+{
+    if (age <= 16)
+    {
+        output = "Mr";
+    }
+    else
+    {
+        output = "Master";
+    }
+}
+
+Console.WriteLine(output);
