11fc96f9092a6b58ff2f81b0223968bb5c17e1eb
imp111 <alexsandartenev@gmail.com>
2024-09-22 16:34:50 +0300

    11 added and done

---
+++ 02. Conditional Statements - Exercises/11.  Number in the range/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = int.Parse(Console.ReadLine());
+string output = string.Empty;
+if (num == 0 || num < -100 || num > 100)
+{
+    output = "No";
+}
+else
+{
+    output = "Yes";
+}
+
+Console.WriteLine(output);
