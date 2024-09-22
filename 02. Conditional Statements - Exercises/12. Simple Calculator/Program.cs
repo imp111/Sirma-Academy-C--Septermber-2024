14cd15173cff21c64a3d7556960430d2e3a42399
imp111 <alexsandartenev@gmail.com>
2024-09-22 18:36:45 +0300

    13. added and done

---
+++ 02. Conditional Statements - Exercises/12. Simple Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+string operation = Console.ReadLine();
+double output = 0.0;
+
+switch (operation.ToLower())
+{
+    case "add": output = a + b; break;
+    case "subtract": output = a - b; break;
+    case "multiply": output = a * b; break;
+    case "divide": output = a / b; break;
+}
+
+Console.WriteLine($"{output:F2}");
---
+++ 02. Conditional Statements - Exercises/13. Vegetable market/Program.cs
@@ -0,0 +1,38 @@
+﻿Dictionary<string, Dictionary<string, double>> products = new Dictionary<string, Dictionary<string, double>>()
+{
+     { "Weekday", new Dictionary<string, double>
+        {
+            { "tomato", 2.50 },
+            { "onion", 1.20 },
+            { "lettuce", 0.85 },
+            { "cucumber", 1.45 },
+            { "pepper", 5.50 }
+        }
+    },
+    { "Weekend", new Dictionary<string, double>
+        {
+            { "tomato", 2.80 },
+            { "onion", 1.30 },
+            { "lettuce", 0.85 },
+            { "cucumber", 1.75 },
+            { "pepper", 3.50 }
+        }
+    }
+};
+
+string vegetable = Console.ReadLine();
+string weekday = Console.ReadLine(); 
+int amount = int.Parse(Console.ReadLine());
+
+switch (weekday.ToLower())
+{
+    case "monday": Console.WriteLine($"{products["Weekday"][vegetable] * amount:F2}"); ; break;
+    case "tuesday": Console.WriteLine($"{products["Weekday"][vegetable] * amount:F2}"); break;
+    case "wednesday": Console.WriteLine($"{products["Weekday"][vegetable] * amount:F2}"); break;
+    case "thursday": Console.WriteLine($"{products["Weekday"][vegetable] * amount:F2}"); break;
+    case "friday": Console.WriteLine($"{products["Weekday"][vegetable] * amount:F2}"); break;
+    case "saturday": Console.WriteLine($"{products["Weekend"][vegetable] * amount:F2}"); break;
+    case "sunday": Console.WriteLine($"{products["Weekend"][vegetable] * amount:F2}"); break;
+    default: Console.WriteLine("error"); break;
+}
+
