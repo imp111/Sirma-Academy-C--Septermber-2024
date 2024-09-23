ae5ad295f50c92078ebbb187192f782f7589d5a9
imp111 <alexsandartenev@gmail.com>
2024-09-23 19:36:56 +0300

    14. added and done

---
+++ 02. Conditional Statements - Exercises/14. Holiday/Program.cs
@@ -0,0 +1,58 @@
+﻿double budget = double.Parse(Console.ReadLine());
+
+while (budget < 10 && budget > 5000)
+{
+    Console.Write("Try different budget!\nBudget: ");
+    budget = double.Parse(Console.ReadLine());
+}
+
+string season = Console.ReadLine();
+
+while (season.ToLower() != "winter" && season.ToLower() != "summer")
+{
+    Console.Write("Try using a valid season (winter/summer)!\nSeason: ");
+    season = Console.ReadLine();
+}
+
+double spent = 0.0;
+string rest = string.Empty;
+
+if (budget <= 100)
+{
+    if (season == "winter")
+    {
+        spent = budget * 0.7;
+        rest = "Hotel";
+    }
+    else
+    {
+        spent = budget * 0.3; 
+        rest = "Camp";
+    }
+      
+    Console.WriteLine("Somewhere in Bulgaria");
+    Console.WriteLine($"{rest} - {spent:F2}");
+}
+else if (budget > 100 &&  budget <= 1000)
+{
+    if (season == "winter")
+    {
+        spent = budget * 0.8;
+        rest = "Hotel";
+    }
+    else
+    {
+        spent = budget * 0.4;
+        rest = "Camp";
+    }
+
+    Console.WriteLine("Somewhere in Europe");
+    Console.WriteLine($"{rest} - {spent:F2}");
+}
+else
+{
+    spent = budget * 0.9;
+
+    Console.WriteLine("Somewhere in Asia");
+    Console.WriteLine($"Hotel - {spent:F2}");
+}
