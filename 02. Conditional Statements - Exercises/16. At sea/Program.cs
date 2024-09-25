96f3ec4c13cea0f30ad4eef5ac82673712351fc5
imp111 <alexsandartenev@gmail.com>
2024-09-25 16:23:56 +0300

    16. added - not finished

---
+++ 02. Conditional Statements - Exercises/16. At sea/Program.cs
@@ -0,0 +1,44 @@
+﻿int days = int.Parse(Console.ReadLine());
+string typeOfRoom = Console.ReadLine();
+string assessment = Console.ReadLine();
+
+switch (typeOfRoom.toLower())
+{
+    case "apartment":
+        totalPrice = (days - 1) * 50.00;
+        
+        if (days < 10)
+        {
+            totalPrice = totalPrice - totalPrice * 30 / 100;
+        }
+        else if (days <= 15 && days >= 10)
+        {
+            totalPrice = totalPrice - totalPrice * 35 / 100;
+        }
+        else 
+        {
+            totalPrice = totalPrice - totalPrice * 50 / 100;
+        }
+
+        break;
+    case "single room":
+        totalPrice = (days - 1) * 25.00;
+        break;
+    case "presidential":
+        totalPrice = (days - 1) * 100.00;
+
+        if (days < 10)
+        {
+            totalPrice = totalPrice - totalPrice * 10 / 100;
+        }
+        else if (days <= 15 && days >= 10)
+        {
+            totalPrice = totalPrice - totalPrice * 15 / 100;
+        }
+        else 
+        {
+            totalPrice = totalPrice - totalPrice * 20 / 100;
+        }
+
+        break;
+}
